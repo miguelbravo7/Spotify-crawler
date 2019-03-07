9e58562caf4dc1f1f730274cb3a1edcb95b1701b
miguelbravo7 <alu0101031538@ull.edu.es>
2019-03-07 19:11:48 +0000

    Peticion multiple implementada

--- spotify_json.py
+++ spotify_json.py
@@ -1,8 +1,15 @@
 import requests
 import json
 import csv
+import sys
 
 dos_primeras = 0
+peticiones = 0
+acc_keys = []
+acc_rows = []
+acc_array = []
+
+
 
 with open('regional-us-daily-2019-02-25.csv','r') as userFile:
     with open('output.csv', 'w') as csvoutput:
@@ -10,7 +17,7 @@ with open('regional-us-daily-2019-02-25.csv','r') as userFile:
         userFileReader = csv.reader(userFile)
         
         for row in userFileReader:
-            
+            peticiones += 1
             if(dos_primeras<2):
                 dos_primeras=dos_primeras+1
                 writer.writerow(row)
@@ -18,21 +25,39 @@ with open('regional-us-daily-2019-02-25.csv','r') as userFile:
             
             song_id=row[-1].split('/')
             #print(song_id[-1])
-            url = 'https://api.spotify.com/v1/audio-features/' + song_id[-1]
-            headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Accept-Charset': 'UTF-8', 'Authorization': 'Bearer BQA86i8n84H4-_rEsk6kYea4oFXd7uIJO-JG5nDXz1UPh0TaDOlx4QiVYybVOKkTUXvpf1iWexsvyV1chrBmE-5A07RAFIWh5H3bfek2kXoA8VPeD7A0m3I8t9ca8EcjiNO7MaAo5-uD7A'}
+            acc_keys.append(song_id[-1])
+            acc_rows.append(row)
+            if(peticiones < 90):
+                continue
+            
+##            print(acc_rows)
+            
+            url = 'https://api.spotify.com/v1/audio-features/?ids=' + "%2C".join(acc_keys)
+            headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Accept-Charset': 'UTF-8', 'Authorization': 'Bearer BQC6lfA8c7lTu0bUideIi2gavCIYRk_OZqmMbyb8lVEU8mxqHEhCJsslamzCp4w3wPp8CYxj58-z3SCUc-jrhrEt01GQW_OsMRtwASrJMpWadUF17dxNrNV5mBDQMkgT9HqRPOm-JzZO3Q'}
             r = requests.get(url,  headers=headers)
 
             data=json.loads(r.text)
-            array = []
+##            print(data['audio_features'])
             
-            for key in data.keys():
-                if key in("type", "id", "uri", "track_href", "analysis_url"):
-                    continue
-                array.append(data[key])
-                
-            #print(array)
-            row.extend(array)
-            writer.writerow(row)
+            try:
+                for song in data['audio_features']:
+                    array = []
+                    for key in song.keys():
+                        if key in("type", "id", "uri", "track_href", "analysis_url"):
+                            continue
+                        array.append(song[key])
+                    acc_array.append(array)
+    ##            print(acc_array)
+                for lineas in acc_rows:
+                    features = acc_array.pop(0)
+                    print(features)
+                    lineas.extend(features)
+    ##                print(lineas)
+                    writer.writerow(lineas)
+            except:
+                print(data)
+                print('https://api.spotify.com/v1/audio-features/?ids=' + "%2C".join(acc_keys))
+            
 
         
 
